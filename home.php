39c7574735331c07b13fdde414f7a90692c63f48
Avanthikaselvakumar <137874781+Avanthikaselvakumar@users.noreply.github.com>
2023-06-27 19:01:53 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,18 @@
+<?php include("head.html")
+?>
+<body>
+<div class="home">
+<div class="img">
+<img src=""C:\Users\hp\Desktop\Contact-page-Book-an-Appointment_Mobile-banner_578X364-px-02.jpg"" height="300px" width="1000px" />
+</div>
+<div class="heading">
+<h1>DOCTOR APPOINTMENT</h1>
+</div>
+<div class="para">
+<p>allows patients to book an appointment from the comfort of their homes, using their computer, laptop or mobile, and at any time. No matter where they are, they can contact doctors of their choice in any location. Even when they are traveling they can consult with their doctor. 
+</p>
+</div>
+</div>
+</body>
+<?php include("foot.html")
+?>
